dd00fadf346ecd2fec575662c23a48554cac947a
drblast <drblast@users.sourceforge.net>
2006-11-30 16:35:51 +0000

    git-svn-id: https://svn.code.sf.net/p/kidbasic/code/branches/devel@167 deda80ff-9d1e-0410-aade-8f0ad8a54eb3

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -67,7 +67,7 @@ MainWindow::MainWindow(QWidget * parent, Qt::WindowFlags f)
   	QLabel *aboutlabel = new QLabel(QObject::tr("<h2 align='center'>BASIC-256 -- Version 0.9</h2> \
 					<p>Copyright &copy; 2006, The BASIC-256 Team</p>	\
 					<p>Please see the CONTRIBUTORS file for a list of developers and translators for this project.</p>\
-					<p><i>You should have received a copy of the GNU General Public License along<br> \
+o				<p><i>You should have received a copy of the GNU General Public License along<br> \
 					with this program; if not, write to the Free Software Foundation, Inc.,<br> \
 					51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.</i></p>"), aboutdialog);
   	QGridLayout *aboutgrid = new QGridLayout();
